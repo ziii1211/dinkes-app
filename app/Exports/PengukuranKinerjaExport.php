dc2884b638bf1759c941795353747c9b799230e2
ziii1211 <ahmdfuzii123@gmail.com>
2026-01-01 12:50:05 +0800

    update

--- app/Exports/PengukuranKinerjaExport.php
+++ app/Exports/PengukuranKinerjaExport.php
@@ -6,11 +6,15 @@ use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents; // Tambahkan ini
+use Maatwebsite\Excel\Events\AfterSheet;   // Tambahkan ini
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 
-class PengukuranKinerjaExport implements FromView, ShouldAutoSize, WithStyles
+class PengukuranKinerjaExport implements FromView, ShouldAutoSize, WithStyles, WithColumnWidths, WithEvents
 {
     protected $pk;
     protected $rencanaAksis;
@@ -33,45 +37,112 @@ class PengukuranKinerjaExport implements FromView, ShouldAutoSize, WithStyles
             5 => 'Mei', 6 => 'Juni', 7 => 'Juli', 8 => 'Agustus', 
             9 => 'September', 10 => 'Oktober', 11 => 'November', 12 => 'Desember'
         ];
-        
         $this->namaBulan = $months[$bulan] ?? 'Bulan';
-        
-        // Format tanggal cetak (misal: 30 Desember 2025)
-        $this->tanggalCetak = date('d') . ' ' . ($months[date('n')] ?? '') . ' ' . date('Y');
+        // Format Tanggal: Banjarmasin, 31 Desember 2026
+        $this->tanggalCetak = 'Banjarmasin, ' . date('t', strtotime("$tahun-$bulan-01")) . ' ' . ($months[$bulan] ?? '') . ' ' . $tahun;
+    }
+
+    public function columnWidths(): array
+    {
+        // Menyesuaikan lebar kolom agar mirip format laporan
+        return [
+            'A' => 5,   // No
+            'B' => 30,  // Kinerja Utama
+            'C' => 35,  // Indikator
+            'D' => 15,  // Capaian Tahun Lalu
+            'E' => 15,  // Satuan
+            'F' => 12,  // Target
+            'G' => 12,  // Realisasi
+            'H' => 12,  // Capaian
+            'I' => 12,  // Target Akhir
+            'J' => 20,  // Capaian Kinerja
+        ];
     }
 
     public function styles(Worksheet $sheet)
     {
-        // 1. Set Default Font: Arial 10
+        // 1. Set Global Font ke Arial 10
         $sheet->getParent()->getDefaultStyle()->getFont()->setName('Arial');
         $sheet->getParent()->getDefaultStyle()->getFont()->setSize(10);
         
-        // 2. Default Alignment: Vertical Center & Wrap Text (Penting agar teks panjang turun ke bawah)
-        $sheet->getParent()->getDefaultStyle()->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+        // 2. Alignment Global: Top & Wrap Text
+        $sheet->getParent()->getDefaultStyle()->getAlignment()->setVertical(Alignment::VERTICAL_TOP);
         $sheet->getParent()->getDefaultStyle()->getAlignment()->setWrapText(true);
 
         return [
-            // Style untuk Judul Utama (Baris 2)
+            // Style Judul Laporan (Baris 2)
             2 => [
-                'font' => ['bold' => true, 'size' => 14],
-                'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER],
+                'font' => ['bold' => true, 'size' => 12],
+                'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER],
             ],
-            
-            // Style untuk Header Tabel (Baris 6 dan 7)
-            6 => [
+            // Style Header Tabel (Baris 6 dan 7)
+            '6:7' => [
                 'font' => ['bold' => true],
                 'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER],
-                'borders' => [
-                    'allBorders' => ['borderStyle' => Border::BORDER_THIN],
+                'fill' => [
+                    'fillType' => Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => 'D9D9D9'] // Abu-abu muda
                 ],
             ],
-            7 => [
-                'font' => ['bold' => true],
-                'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER],
-                'borders' => [
-                    'allBorders' => ['borderStyle' => Border::BORDER_THIN],
-                ],
-            ],
+        ];
+    }
+
+    /**
+     * Menggunakan Event untuk memberikan Border ke seluruh tabel data secara dinamis
+     */
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+                
+                // Cari baris terakhir yang berisi data (mengabaikan footer tanda tangan)
+                // Kita akan mencari baris di mana kolom No (A) atau kolom Kinerja (B) terakhir terisi dalam konteks tabel
+                // Namun cara paling aman untuk "kotak per data" adalah mendeteksi range tabel.
+                
+                $highestRow = $sheet->getHighestRow();
+                
+                // Asumsi: Kita mencari baris sebelum tulisan "Penjelasan per Indikator Kinerja"
+                // Atau kita loop dari bawah ke atas.
+                $lastDataRow = 7; // Default minimal header
+                
+                for ($row = 8; $row <= $highestRow; $row++) {
+                    $cellValue = $sheet->getCell('A' . $row)->getValue();
+                    $cellValueB = $sheet->getCell('B' . $row)->getValue();
+                    
+                    // Stop jika ketemu footer "Penjelasan..."
+                    if (str_contains((string)$cellValue, 'Penjelasan per Indikator') || str_contains((string)$cellValueB, 'Penjelasan per Indikator')) {
+                        break;
+                    }
+                    $lastDataRow = $row;
+                }
+
+                // Terapkan Border All (Kotak) ke seluruh tabel data
+                $tableRange = 'A6:J' . $lastDataRow;
+                $sheet->getStyle($tableRange)->applyFromArray([
+                    'borders' => [
+                        'allBorders' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                            'color' => ['rgb' => '000000'],
+                        ],
+                    ],
+                ]);
+
+                // Styling khusus untuk baris "Aktifitas" (Background abu-abu tipis)
+                // Kita loop ulang untuk mencari header aktifitas
+                for ($row = 8; $row <= $lastDataRow; $row++) {
+                    $valC = $sheet->getCell('C' . $row)->getValue();
+                    if ($valC == 'Aktifitas yang berhubungan dengan Indikator') {
+                        $sheet->getStyle('A' . $row . ':J' . $row)->applyFromArray([
+                            'fill' => [
+                                'fillType' => Fill::FILL_SOLID,
+                                'startColor' => ['rgb' => 'F2F2F2'] // Abu-abu sangat muda
+                            ],
+                            'font' => ['bold' => true, 'italic' => true]
+                        ]);
+                    }
+                }
+            },
         ];
     }
 
@@ -83,7 +154,8 @@ class PengukuranKinerjaExport implements FromView, ShouldAutoSize, WithStyles
             'jabatan' => $this->jabatan,
             'tahun' => $this->tahun,
             'bulan' => $this->bulan,
-            'namaBulan' => $this->namaBulan,
+            'namaBulan' => strtoupper($this->namaBulan),
+            'namaBulanKecil' => $this->namaBulan,
             'tanggalCetak' => $this->tanggalCetak
         ]);
     }
--- resources/views/cetak/pengukuran-kinerja-excel.blade.php
+++ resources/views/cetak/pengukuran-kinerja-excel.blade.php
@@ -3,20 +3,9 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <style>
-        .title { font-size: 14px; font-weight: bold; text-align: center; }
-        .table-header { 
-            font-weight: bold; 
-            text-align: center; 
-            vertical-align: middle;
-            border: 1px solid #000000;
-            background-color: #d1d5db; /* Abu-abu muda */
-        }
-        .table-cell { 
-            border: 1px solid #000000; 
-            vertical-align: top; 
-            padding: 5px; 
-        }
-        .center { text-align: center; vertical-align: middle; }
+        /* Kita minimalkan style CSS karena styling utama dilakukan via Export Class PHP */
+        .text-center { text-align: center; vertical-align: middle; }
+        .text-top { vertical-align: top; }
         .text-bold { font-weight: bold; }
     </style>
 </head>
@@ -24,11 +13,11 @@
     {{-- BARIS 1: Kosong --}}
     <table><tr><td></td></tr></table>
 
-    {{-- BARIS 2: JUDUL LAPORAN --}}
+    {{-- BARIS 2: JUDUL --}}
     <table>
         <tr>
-            <td colspan="10" class="title" style="text-align: center; font-weight: bold; font-size: 14pt; height: 30px; vertical-align: middle;">
-                LAPORAN PENGUKURAN KINERJA BULAN S/D BULAN {{ strtoupper($namaBulan) }} TAHUN {{ $tahun }}
+            <td colspan="10" style="text-align: center; font-weight: bold; font-size: 12pt;">
+                LAPORAN PENGUKURAN KINERJA BULAN S/D BULAN {{ $namaBulan }} TAHUN {{ $tahun }}
             </td>
         </tr>
     </table>
@@ -36,7 +25,7 @@
     {{-- BARIS 3: Kosong --}}
     <table><tr><td></td></tr></table>
 
-    {{-- BARIS 4 & 5: IDENTITAS SKPD --}}
+    {{-- BARIS 4 & 5: IDENTITAS --}}
     <table>
         <tr>
             <td colspan="2" style="font-weight: bold;">NAMA SKPD</td>
@@ -49,120 +38,127 @@
     </table>
 
     {{-- TABEL UTAMA --}}
-    <table border="1">
+    <table border="1"> {{-- Border=1 membantu excel mendeteksi grid dasar --}}
         <thead>
-            {{-- Header Baris 1 --}}
             <tr>
-                <th rowspan="2" class="table-header" width="5">No.</th>
-                <th rowspan="2" class="table-header" width="40">Kinerja Utama</th>
-                <th rowspan="2" class="table-header" width="45">Indikator</th>
-                <th rowspan="2" class="table-header" width="15">Capaian Tahun Lalu</th>
-                <th rowspan="2" class="table-header" width="15">Satuan Indikator Kinerja Utama</th>
-                <th colspan="3" class="table-header">TARGET DAN CAPAIAN</th>
-                <th rowspan="2" class="table-header" width="15">Target Akhir Renstra</th>
-                <th rowspan="2" class="table-header" width="20">capaian kinerja bulan {{ $namaBulan }}<br/>(realisasi:targetx100%)</th>
+                <th rowspan="2" width="5">No.</th>
+                <th rowspan="2" width="30">Kinerja Utama</th>
+                <th rowspan="2" width="35">Indikator</th>
+                <th rowspan="2" width="15">Capaian Tahun Lalu</th>
+                <th rowspan="2" width="15">Satuan Indikator Kinerja Utama</th>
+                <th colspan="3">TARGET DAN CAPAIAN</th>
+                <th rowspan="2" width="15">Target Akhir Renstra</th>
+                <th rowspan="2" width="20">capaian kinerja bulan {{ $namaBulanKecil }}<br/>(realisasi:targetx100%)</th>
             </tr>
-            {{-- Header Baris 2 --}}
             <tr>
-                <th class="table-header" width="15">Target</th>
-                <th class="table-header" width="15">Realisasi s.d Bulan ini</th>
-                <th class="table-header" width="15">% Capaian</th>
+                <th width="12">Target</th>
+                <th width="12">Realisasi s.d Bulan ini</th>
+                <th width="12">% Capaian</th>
             </tr>
         </thead>
         <tbody>
             @if($pk && $pk->sasarans->count() > 0)
                 @php $no = 1; @endphp
                 @foreach($pk->sasarans as $sasaran)
-                    @php 
-                        // Hitung jumlah indikator untuk rowspan
-                        $rowspan = $sasaran->indikators->count(); 
-                        if($rowspan == 0) $rowspan = 1;
-                    @endphp
-
+                    {{-- Loop Indikator --}}
                     @foreach($sasaran->indikators as $index => $ind)
                         <tr>
-                            {{-- Kolom No & Sasaran (Hanya muncul di baris pertama tiap sasaran) --}}
+                            {{-- No & Kinerja Utama (Grouping) --}}
                             @if($index === 0)
-                                <td class="table-cell center" rowspan="{{ $rowspan }}">{{ $no++ }}.</td>
-                                <td class="table-cell" rowspan="{{ $rowspan }}">{{ $sasaran->sasaran }}</td>
+                                @php $rowspan = $sasaran->indikators->count(); @endphp
+                                <td align="center" rowspan="{{ $rowspan }}" valign="top">{{ $no++ }}.</td>
+                                <td rowspan="{{ $rowspan }}" valign="top">{{ $sasaran->sasaran }}</td>
                             @endif
 
-                            {{-- Kolom Indikator --}}
-                            <td class="table-cell">- {{ $ind->nama_indikator }}</td>
-
-                            {{-- Capaian Tahun Lalu (Data Dummy/Placeholder) --}}
-                            <td class="table-cell center">100</td> 
-
-                            {{-- Satuan --}}
-                            <td class="table-cell center">{{ $ind->satuan }}</td>
-
-                            {{-- Target Tahunan --}}
-                            <td class="table-cell center">{{ $ind->target_tahunan }}</td>
-
-                            {{-- Realisasi --}}
-                            <td class="table-cell center">{{ $ind->realisasi_bulan ?? '0' }}</td>
-
-                            {{-- % Capaian --}}
-                            <td class="table-cell center">
+                            {{-- Data Indikator --}}
+                            <td valign="top">- {{ $ind->nama_indikator }}</td>
+                            <td align="center" valign="top">100</td> {{-- Dummy --}}
+                            <td align="center" valign="top">{{ $ind->satuan }}</td>
+                            <td align="center" valign="top">{{ $ind->target_tahunan }}</td>
+                            <td align="center" valign="top">{{ $ind->realisasi_bulan ?? '0' }}</td>
+                            <td align="center" valign="top">
                                 @if(isset($ind->capaian_bulan) && is_numeric($ind->capaian_bulan))
-                                    {{ round((float)$ind->capaian_bulan, 2) }}
-                                @else
-                                    0
-                                @endif
+                                    {{ round($ind->capaian_bulan, 2) }}
+                                @else 0 @endif
                             </td>
-
-                            {{-- Target Akhir Renstra (Ambil target tahun terakhir atau dummy) --}}
-                            <td class="table-cell center">{{ $ind->target_2030 ?? $ind->target_2029 ?? $ind->target_tahunan }}</td>
-
-                            {{-- Capaian Kinerja (Sama dengan % Capaian) --}}
-                            <td class="table-cell center">
+                            <td align="center" valign="top">{{ $ind->target_2030 ?? $ind->target_tahunan }}</td>
+                            <td align="center" valign="top">
                                 @if(isset($ind->capaian_bulan) && is_numeric($ind->capaian_bulan))
-                                    {{ round((float)$ind->capaian_bulan, 2) }}
-                                @else
-                                    0
-                                @endif
+                                    {{ round($ind->capaian_bulan, 2) }}
+                                @else 0 @endif
                             </td>
                         </tr>
                     @endforeach
-                    
-                    {{-- Handle jika sasaran tidak punya indikator --}}
-                    @if($sasaran->indikators->count() == 0)
-                        <tr>
-                            <td class="table-cell center">{{ $no++ }}.</td>
-                            <td class="table-cell">{{ $sasaran->sasaran }}</td>
-                            <td class="table-cell">-</td>
-                            <td class="table-cell center">-</td>
-                            <td class="table-cell center">-</td>
-                            <td class="table-cell center">-</td>
-                            <td class="table-cell center">-</td>
-                            <td class="table-cell center">-</td>
-                            <td class="table-cell center">-</td>
-                            <td class="table-cell center">-</td>
-                        </tr>
-                    @endif
                 @endforeach
             @else
                 <tr>
-                    <td colspan="10" class="table-cell center">Data Kinerja belum tersedia untuk periode ini.</td>
+                    <td colspan="10" align="center">Data Kinerja tidak ditemukan</td>
+                </tr>
+            @endif
+
+            {{-- BAGIAN AKTIFITAS / RENCANA AKSI --}}
+            {{-- Header Aktifitas --}}
+            <tr>
+                <td></td>
+                <td></td>
+                <td style="font-weight: bold; font-style: italic;">Aktifitas yang berhubungan dengan Indikator</td>
+                <td align="center" style="font-weight: bold; font-style: italic;">Capaian Aktifitas Tahun Lalu</td>
+                <td align="center" style="font-weight: bold; font-style: italic;">Satuan Indikator Aktifitas</td>
+                <td align="center" style="font-weight: bold; font-style: italic;">Target aktifitas</td>
+                <td align="center" style="font-weight: bold; font-style: italic;">realisasi aktifitas</td>
+                <td align="center" style="font-weight: bold; font-style: italic;">capaian aktifitas</td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            {{-- Loop Rencana Aksi --}}
+            @if(isset($rencanaAksis) && count($rencanaAksis) > 0)
+                @foreach($rencanaAksis as $aksi)
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td valign="top">- {{ $aksi->nama_aksi }}</td>
+                        <td align="center" valign="top">-</td>
+                        <td align="center" valign="top">{{ $aksi->satuan }}</td>
+                        <td align="center" valign="top">{{ $aksi->target }}</td>
+                        
+                        @php
+                            $realisasiAksi = $aksi->realisasi_bulan ?? 0;
+                            $capaianAksi = 0;
+                            if(is_numeric($aksi->target) && $aksi->target > 0 && is_numeric($realisasiAksi)){
+                                $capaianAksi = ($realisasiAksi / $aksi->target) * 100;
+                            }
+                        @endphp
+
+                        <td align="center" valign="top">{{ $realisasiAksi }}</td>
+                        <td align="center" valign="top">{{ round($capaianAksi, 2) }}</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            @else
+                 <tr>
+                    <td></td>
+                    <td></td>
+                    <td style="color: gray; font-style: italic;">(Tidak ada aktivitas terdaftar)</td>
+                    <td colspan="7"></td>
                 </tr>
             @endif
         </tbody>
     </table>
 
-    {{-- BAGIAN PENJELASAN & REKOMENDASI --}}
+    {{-- BAGIAN PENJELASAN (DI LUAR TABEL DATA) --}}
     <table>
-        <tr><td colspan="10"></td></tr> {{-- Spasi --}}
-        
+        <tr><td colspan="10"></td></tr>
         <tr>
             <td colspan="10" style="font-weight: bold;">Penjelasan per Indikator Kinerja</td>
         </tr>
-        
         <tr>
             <td colspan="10" style="font-weight: bold;">Upaya :</td>
         </tr>
         <tr><td colspan="10">1. ....................................................................................................</td></tr>
         <tr><td colspan="10">2. ....................................................................................................</td></tr>
-
+        
         <tr>
             <td colspan="10" style="font-weight: bold;">Hambatan :</td>
         </tr>
@@ -182,16 +178,14 @@
     <table>
         <tr>
             <td colspan="5"></td>
-            <td colspan="5" align="center">Banjarmasin, {{ $tanggalCetak }}</td>
+            <td colspan="5" align="center">{{ $tanggalCetak }}</td>
         </tr>
         <tr>
             <td colspan="5" align="center">Yang Melaporkan</td>
             <td colspan="5" align="center">Yang Melaporkan</td>
         </tr>
         <tr>
-            {{-- KIRI: Contoh Sekretaris / Atasan --}}
-            <td colspan="5" align="center">Sekretaris,</td> 
-            {{-- KANAN: Jabatan Pemilik Laporan --}}
+            <td colspan="5" align="center">Sekretaris,</td>
             <td colspan="5" align="center">{{ $jabatan->nama }}</td>
         </tr>
         <tr>
@@ -199,12 +193,10 @@
             <td colspan="5" align="center">Dinas Kesehatan Provinsi Kalimantan Selatan</td>
         </tr>
         <tr>
-            <td colspan="10" height="60"></td> {{-- Spasi TTD --}}
+            <td colspan="10" height="60"></td>
         </tr>
         <tr>
-            <td colspan="5" align="center" style="font-weight: bold; text-decoration: underline;">
-                (Nama Atasan)
-            </td>
+            <td colspan="5" align="center" style="font-weight: bold; text-decoration: underline;">(Nama Atasan)</td>
             <td colspan="5" align="center" style="font-weight: bold; text-decoration: underline;">
                 {{ $jabatan->pegawai->nama ?? '..........................' }}
             </td>
@@ -215,18 +207,19 @@
         </tr>
         <tr>
             <td colspan="5" align="center">NIP. ..........................</td>
-            <td colspan="5" align="center">NIP. {{ $jabatan->pegawai->nip ?? '..........................' }}</td>
+            <td colspan="5" align="center">
+                NIP. {{ $jabatan->pegawai->nip ?? '..........................' }}
+            </td>
         </tr>
     </table>
-
+    
     <br>
+    
+    {{-- KETERANGAN --}}
     <table>
-        <tr>
-            <td colspan="10" style="font-style: italic;">keterangan:</td>
-        </tr>
-        <tr>
-            <td colspan="10" style="font-style: italic;">"- indikator kinerja dimasukkan semua dalam satu kolom, disesuaikan dengan kinerja utama"</td>
-        </tr>
+        <tr><td colspan="10" style="font-style: italic;">keterangan:</td></tr>
+        <tr><td colspan="10" style="font-style: italic;">"- indikator kinerja dimasukkan semua dalam satu kolom, disesuaikan dengan kinerja utama"</td></tr>
+        <tr><td colspan="10" style="font-style: italic;">"- untuk uraian aktifitas per indikator kinerja, sesuai dengan diisi dalam edialog (ren aksi)"</td></tr>
     </table>
 </body>
 </html>
