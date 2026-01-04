b3810520ff8148e393c0ab414ac7e99739a8c2ff
ziii1211 <ahmdfuzii123@gmail.com>
2026-01-05 01:02:25 +0800

    update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dinas kesehatan{{ isset($title) && $title ? ' . '.$title : '' }}</title>
-    <link rel="icon" href="{{ asset('logo pemprov.png') }}" type="image/png">
+    
+    {{-- PERUBAHAN DISINI: Mengganti logo favicon --}}
+    <link rel="icon" href="{{ asset('Coat_of_arms_of_South_Kalimantan.svg.png') }}" type="image/png">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
